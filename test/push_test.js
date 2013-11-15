61ea03b0bc0ef5dcd792ac09a3b8257ea3504bc7
Ruben Vermeersch <ruben@rocketeer.be>
2013-11-15 11:13:03 +0100

    Add gitpush.
    Fixes #2.

---
+++ test/push_test.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var command = require('../lib/command_push');
+var Test = require('./_common');
+
+describe('push', function () {
+    it('should push branch', function (done) {
+        var options = {
+        };
+
+        new Test(command, options)
+            .expect(["push", "origin"])
+            .run(done);
+    });
+
+    it('should accept --tags option', function (done) {
+        var options = {
+            tags: true
+        };
+
+        new Test(command, options)
+            .expect(["push", "--tags", "origin"])
+            .run(done);
+    });
+
+    it('should accept --all option', function (done) {
+        var options = {
+            all: true
+        };
+
+        new Test(command, options)
+            .expect(["push", "--all", "origin"])
+            .run(done);
+    });
+
+    it('should accept branch option', function (done) {
+        var options = {
+            branch: 'master'
+        };
+
+        new Test(command, options)
+            .expect(["push", "origin", "master"])
+            .run(done);
+    });
+
+    it('should accept --set-upstream option', function (done) {
+        var options = {
+            upstream: true,
+            branch: 'master'
+        };
+
+        new Test(command, options)
+            .expect(["push", "--set-upstream", "origin", "master"])
+            .run(done);
+    });
+});
