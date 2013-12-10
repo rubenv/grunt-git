14664133e59a820000f5057c6985d855d6c5d501
Sai Gunturi <sai@nectarom.com>
2013-12-10 11:20:33 -0600

    Add merge test file from rebase tests.

---
+++ test/merge_test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var command = require('../lib/command_rebase');
+var Test = require('./_common');
+
+describe('rebase', function () {
+    it('should rebase', function (done) {
+        var options = {
+            branch: 'origin/master'
+        };
+
+        new Test(command, options)
+            .expect(["rebase", "origin/master"])
+            .run(done);
+    });
+
+    it('should allow choosing strategy', function (done) {
+        var options = {
+            branch: 'master',
+            theirs: true
+        };
+
+        new Test(command, options)
+            .expect(["rebase", "--strategy=recursive", "-Xtheirs", "master"])
+            .run(done);
+    });
+});
