f265109cfc44244de30e16b17880ce95848e9675
Dylan Wood <dylancwood@gmail.com>
2014-06-22 11:05:16 -0700

    added additional options to merge task with tests

--- test/merge_test.js
+++ test/merge_test.js
@@ -46,4 +46,59 @@ describe('merge', function () {
             .expect(['merge', 'origin/master', '--squash'])
             .run(done);
     });
+
+    it('should accept --edit option', function (done) {
+        var options = {
+            branch: 'origin/master',
+            edit: true
+        };
+
+        new Test(command, options)
+            .expect(['merge', 'origin/master', '--edit'])
+            .run(done);
+    });
+
+    it('should accept --no-edit option', function (done) {
+        var options = {
+            branch: 'origin/master',
+            noEdit: true
+        };
+
+        new Test(command, options)
+            .expect(['merge', 'origin/master', '--no-edit'])
+            .run(done);
+    });
+
+    it('should accept --message option', function (done) {
+        var options = {
+            branch: 'origin/master',
+            message: 'test message'
+        };
+
+        new Test(command, options)
+            .expect(['merge', 'origin/master', '-m', 'test message'])
+            .run(done);
+    });
+
+    it('should accept --commit option', function (done) {
+        var options = {
+            branch: 'origin/master',
+            commit: true
+        };
+
+        new Test(command, options)
+            .expect(['merge', 'origin/master', '--commit'])
+            .run(done);
+    });
+
+    it('should accept --no-commit option', function (done) {
+        var options = {
+            branch: 'origin/master',
+            noCommit: true
+        };
+
+        new Test(command, options)
+            .expect(['merge', 'origin/master', '--no-commit'])
+            .run(done);
+    });
 });
