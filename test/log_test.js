9cb9af6bf30286143ac2e4496a75f4aef7411894
Dylan Wood <dylancwood@gmail.com>
2014-11-12 04:02:24 -0800

    modified use of flopmang and added tests

--- test/log_test.js
+++ test/log_test.js
@@ -5,12 +5,21 @@ var Test = require('./_common');
 
 
 describe('log', function () {
-    it('should log', function (done) {
+    it('should log with default pretty format', function (done) {
         var options = {
             noMerges: false
         };
         new Test(command, options)
             .expect(['log', '--pretty=format:' + command.format], [null, { stdout: '' }])
+            .run(done);
+    });
+    it('should log with specified pretty format', function (done) {
+        var customFormat = 'format:some custom format';
+        var options = {
+            pretty: customFormat
+        };
+        new Test(command, options)
+            .expect(['log', '--pretty=' + customFormat, '--no-merges'], [null, { stdout: '' }])
             .run(done);
     });
     it('should log the specified number of logs', function (done) {
@@ -30,22 +39,44 @@ describe('log', function () {
             .expect(['log', '--pretty=format:' + command.format, '--no-merges'], [null, { stdout: '' }])
             .run(done);
     });
-    it('should order by date if --before or --after is used', function (done) {
-        var before = new Date();
-        var after = new Date();
+    it('should log merges if noMerges is not defined', function (done) {
         var options = {
-            noMerges: false,
-            before: before,
-            after: after
         };
         new Test(command, options)
-            .expect([
-                'log',
-                '--pretty=format:' + command.format,
-                '--before', before.toISOString(),
-                '--after', after.toISOString(),
-                '--date-order'
-            ], [null, { stdout: '' }])
+            .expect(['log', '--pretty=format:' + command.format, '--no-merges'], [null, { stdout: '' }])
+            .run(done);
+    });
+    it('should order by date is dateOrder is defined', function (done) {
+        var options = {
+        };
+        new Test(command, options)
+            .expect(['log', '--pretty=format:' + command.format, '--no-merges'], [null, { stdout: '' }])
+            .run(done);
+    });
+    it('should order by date if --before or --after is used', function (done) {
+        var options = {
+            noMerges: false,
+            dateOrder: true
+        };
+        new Test(command, options)
+            .expect(['log', '--pretty=format:' + command.format, '--date-order'], [null, { stdout: '' }])
+            .run(done);
+    });
+    it('should log from a specified branch to HEAD if from is used', function (done) {
+        var options = {
+            from: '1234'
+        };
+        new Test(command, options)
+            .expect(['log', '--pretty=format:' + command.format, '--no-merges', '1234..HEAD'], [null, { stdout: '' }])
+            .run(done);
+    });
+    it('should log from a specified branch to a specfied branch if from and to are used', function (done) {
+        var options = {
+            from: '1234',
+            to: '6789'
+        };
+        new Test(command, options)
+            .expect(['log', '--pretty=format:' + command.format, '--no-merges', '1234..6789'], [null, { stdout: '' }])
             .run(done);
     });
 });
