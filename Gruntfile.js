77da358de9a856b1bf6e0b2afa8f53fedd162fa5
db developer <db.developer@gmx.de>
2021-08-12 13:27:11 +0200

    moved configurations to .conf/grunt/*.js. Added load-grunt-config and load-grunt-tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,93 +4,27 @@
  *
  * Copyright (c) 2013 Ruben Vermeersch
  * Licensed under the MIT license.
- */
-
+ *
+ *//* jshint esversion: 6 */
 'use strict';
+
+const path         = require('path');
+const GRUNTCONFDIR = path.join(process.cwd(), '.conf', 'grunt');
 
 module.exports = function (grunt) {
 
-    // Project configuration.
-    grunt.initConfig({
-        watch: {
-            dev: {
-                files: ['tasks/*.js', 'lib/*.js', 'test/*.js'],
-                tasks: ['jshint', 'test']
-            }
-        },
+  require('load-grunt-config')(grunt, {configPath: GRUNTCONFDIR});
+  require('load-grunt-tasks' )(grunt);
 
-        jshint: {
-            all: [
-                'Gruntfile.js',
-                'tasks/*.js',
-                'lib/*.js',
-                'test/*.js',
-                'test/fixtures/*.js'
-            ],
-            options: {
-                jshintrc: '.jshintrc',
-            },
-        },
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
 
-        jscs: {
-            src: {
-                options: {
-                    config: '.jscs.json'
-                },
-                files: {
-                    src: [
-                        'Gruntfile.js',
-                        'tasks/*.js',
-                        'lib/*.js',
-                        'test/*.js',
-                        'test/fixtures/*.js'
-                    ],
-                }
-            },
-        },
 
-        mochacli: {
-            options: {
-                files: 'test/*_test.js'
-            },
-            spec: {
-                options: {
-                    reporter: 'spec',
-                    timeout: 10000
-                }
-            }
-        },
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['test']);
 
-        bump: {
-            options: {
-                files: ['package.json'],
-                commitFiles: ['-a'],
-                pushTo: 'origin'
-            }
-        },
-
-        // Before generating any new files, remove any previously-created files.
-        clean: {
-            tests: ['tmp'],
-        }
-    });
-
-    // Actually load this plugin's task(s).
-    grunt.loadTasks('tasks');
-
-    // These plugins provide necessary tasks.
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-jscs');
-    grunt.loadNpmTasks('grunt-mocha-cli');
-    grunt.loadNpmTasks('grunt-bump');
-
-    // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-    // plugin's task(s), then test the result.
-    grunt.registerTask('test', ['jshint', 'jscs', 'clean', 'mochacli']);
-
-    // By default, lint and run all tests.
-    grunt.registerTask('default', ['test']);
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'jshint', 'nyc_mocha']);
 
 };
